d02e0f4d1ce419a4841d744a1ee2ea20df11a071
user <matthew.aldous@gmail.com>
2025-08-10 23:05:25 +0000

    1754825494

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', function () { return view('welcome'); });
 Route::get('/health', fn() => response()->json(['status' => 'ok'], 200));
+
+?>
